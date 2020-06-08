8fd6d5db8cdc2e78d49a1c57c6e13c10a1ba7bd9
James (WSL) <jamesmccarthy150@gmail.com>
2020-06-08 22:30:48 +0100

    Reloaded the node_modules

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var express = require("express"); var app = express(); app.set("view engine", "ejs")
+var express = require("express"); var app = express()
+var fs = require("fs")
+var cors = require("cors")
+require("dotenv").config()
 
-app.get('/api', (req, res) => { res.render('about');});
+// To Use .env, preface the variable name with process.env.
 
-app.listen(3000);
+app.get("/api", cors(), (req, res, next) => {
+    fs.readFile(__dirname + "/" + "devices.json", "utf-8", (err, data) => {
+        let obj = JSON.parse(data);
+        res.json(obj);
+    })
+})
+
+
+app.listen(1337, () => {
+    console.log("Server running on port 1337");
+});
