e0f44d64f17f622b4d12fc976ad262aa85e5c0d7
James (WSL) <jamesmccarthy150@gmail.com>
2020-06-10 12:44:35 +0100

    tried a 2nd WoL lib

--- controllers/wol.js
+++ controllers/wol.js
@@ -1,10 +1,12 @@
-var wol = require("node-wol");
+var wol = require("wol");
 require("dotenv").config()
 
 module.exports = {
     Wake: function wake() {
         console.log("Sent WoL Packet to James-PC")
-        wol.wake(process.env.PC_MAC_ADDR);
+        wol.wake(process.env.PC_MAC_ADDR, (err, res) => {
+            console.log(res)
+        });
         console.log("Sent WoL to " + process.env.PC_MAC_ADDR)
     }
 }
